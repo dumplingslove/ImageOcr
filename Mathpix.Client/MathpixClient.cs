d41eaa7632f508e2620457d47272c99eb4963e70
Zhangwei Wu <zhangweiwu@outlook.com>
2018-08-23 22:51:09 -0700

    equation ocr

---
+++ Mathpix.Client/MathpixClient.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mathpix.Client
+{
+    public class MathpixClient
+    {
+        private HttpClient client;
+
+        public MathpixClient(string appId, string appKey)
+        {
+            client = new HttpClient
+            {
+                BaseAddress = new Uri("https://api.mathpix.com/v3/latex")
+            };
+
+            client.DefaultRequestHeaders.Add("app_id", appId);
+            client.DefaultRequestHeaders.Add("app_key", appKey);
+            
+        }
+        
+        public async Task<string> GetResultJsonAsync(string path)
+        {
+            using (var stream = File.OpenRead(path))
+            {
+                return await GetResultJsonAsync(stream);
+            }
+        }
+
+        public async Task<string> GetResultJsonAsync(Stream stream)
+        {
+            byte[] data = null;
+            using (MemoryStream ms = new MemoryStream())
+            {
+                stream.CopyTo(ms);
+                data = ms.ToArray();
+            }
+            
+            var content = "{ \"src\" : \"data:image/jpeg;base64," + Convert.ToBase64String(data) + "\" }";
+
+            var response = await client.PostAsync("", new StringContent(content, Encoding.UTF8, "application/json"));
+            response = response.EnsureSuccessStatusCode();
+
+            return await response.Content.ReadAsStringAsync();
+        }
+    }
+}
---
+++ Mathpix.Client/MathpixResult.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace Mathpix.Client
+{
+    public class MathpixResult
+    {
+        [JsonProperty("detection_list")]
+        public string[] DetectionList { get; set; }
+        [JsonProperty("detection_map")]
+        public Dictionary<string, double> DetectionMap { get; set; }
+        [JsonProperty("error")]
+        public string Error { get; set; }
+        [JsonProperty("latex")]
+        public string Latex { get; set; }
+        [JsonProperty("latex_confidence_rate")]
+        public double LatexConfidence { get; set; }
+        [JsonProperty("position")]
+        public MathpixPos Position { get; set; }
+        [JsonProperty("latex_list")]
+        public string[] LatexList { get; set; }
+    }
+
+    public class MathpixPos
+    {
+        [JsonProperty("width")]
+        public int Width { get; set; }
+        [JsonProperty("height")]
+        public int Height { get; set; }
+        [JsonProperty("top_left_x")]
+        public int X { get; set; }
+        [JsonProperty("top_left_y")]
+        public int Y { get; set; }
+    }
+}
