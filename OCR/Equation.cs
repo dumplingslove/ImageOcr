f17c0e11d11baf64290b16e979e1204583ab8795
dumplingslove <darancai@gmail.com>
2018-08-23 21:34:02 -0700

    Add google OCR

---
+++ OCR/Equation.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Google.Cloud.Vision.V1;
+
+namespace OCR
+{
+    public class Equation
+    {
+        public string Content { get; set; }
+
+        public List<Vertex> Vertices { get; set; }
+    }
+}
